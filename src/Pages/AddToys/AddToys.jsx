3dde0c021ef0b31ea7030b70759f77c38bf7ad6e
rakibulhaqueonline <rakibulhaque.info@gmail.com>
2023-05-18 23:52:41 +0600

    Add toy modified

--- src/Pages/AddToys/AddToys.jsx
+++ src/Pages/AddToys/AddToys.jsx
@@ -17,6 +17,8 @@ const AddToys = () => {
         const rating = form.rating.value;
         const description = form.description.value;
         const sellerName = form.sellerName.value;
+        const subCategory = form.subCategory.value;
+        const quantity = form.quantity.value;
         const sellerEmail = user?.email;
         const newToy = {
             photo,
@@ -25,7 +27,9 @@ const AddToys = () => {
             sellerName,
             price,
             description,
-            sellerEmail: user?.email
+            sellerEmail,
+            quantity,
+            subCategory
         }
 
         console.log(newToy);
@@ -54,21 +58,15 @@ const AddToys = () => {
                 <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Seller Name</span>
-                        </label>
-                        <input type="text" name="sellerName" placeholder='Seller Name' className="input input-bordered" />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
                             <span className="label-text">Toy Name</span>
                         </label>
                         <input type="text" name="toyName" placeholder='Toy Name' className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Available Quantity</span>
+                            <span className="label-text">Seller Name</span>
                         </label>
-                        <input type="number" name="name" placeholder='Quantity' className="input input-bordered" />
+                        <input type="text" name="sellerName" placeholder='Seller Name' className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
@@ -78,15 +76,15 @@ const AddToys = () => {
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Email</span>
+                            <span className="label-text">Available Quantity</span>
                         </label>
-                        <input type="text" name="email" defaultValue={user?.email} placeholder="Seller email" className="input input-bordered" />
+                        <input type="number" name="quantity" placeholder='Quantity' className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Price</span>
                         </label>
-                        <input type="text" name='price' className="input input-bordered" />
+                        <input type="text" name='price' placeholder='Price' className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
@@ -96,9 +94,22 @@ const AddToys = () => {
                     </div>
                     <div className="form-control">
                         <label className="label">
+                            <span className="label-text">Sub Category</span>
+                        </label>
+                        <input type="number" name="subCategory" placeholder='Sub Category' className="input input-bordered" />
+                    </div>
+                   
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" name="email" defaultValue={user?.email} placeholder="Seller email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
                             <span className="label-text">Description</span>
                         </label>
-                        <input type="text" name='description' placeholder='Describe about toy' className="input input-bordered" />
+                        <input type="text" name='description' placeholder='Describe about toy' className="w-full input input-bordered" />
                     </div>
                 </div>
                 <div className="mt-6 form-control">
