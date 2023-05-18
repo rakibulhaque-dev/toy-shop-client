448bdef2f04c39c7e8d3b7f25dd4eb261b6fd973
rakibulhaqueonline <rakibulhaque.info@gmail.com>
2023-05-18 11:04:22 +0600

    DaisyUI Connected

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Navbar = () => {
     return (
         <>
-        
-        <div>
-            <h3>ElevenToys</h3>
-        </div>
-        
-        
+
+            <div className='container mx-auto bg-slate-200 rounded shadow-lg'>
+                <h3 className='text-center text-red-400'>ElevenToys</h3>
+            </div>
+
+
         </>
     );
 };
